ff3695566f3347f5e0100ca32dc9ea2f4841da50
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 23:53:24 +0300

    Configure client-side routing with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // index.js
+
+// Import React and ReactDOM for rendering
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Import the main App component
 import App from './App';
+
+// Import BrowserRouter for client-side routing
 import { BrowserRouter } from 'react-router-dom';
 
+// Render the App component within the BrowserRouter
 ReactDOM.render(
   <React.StrictMode>
-    
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    
+    {/* Wrap the App component with BrowserRouter for client-side routing */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
+  // Render the entire application within the root element in the HTML document
   document.getElementById('root')
 );
