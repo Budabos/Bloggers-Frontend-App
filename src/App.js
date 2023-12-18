9dad419b7ed5a7c80805edb8d0d8d2086efbb7f1
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 22:07:49 +0300

    blogger app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import './App.css';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
+import { Routes, Route, Navigate } from 'react-router';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog/:id' element={<Blog />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      
     </div>
   );
-}
+};
 
 export default App;
