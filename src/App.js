e0fcb5e940403d373fe85a5552a79397ea0c1bd5
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 23:12:28 +0300

    Initialize App component with main structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+// Import necessary dependencies and components from React and other modules
 import React from 'react';
+import './App.css';  // Import the CSS file for styling
+import Blog from './pages/Blog';  // Import the Blog component
+import Home from './pages/Home';  // Import the Home component
+import { Routes, Route, Navigate } from 'react-router';  // Import necessary components from react-router
 
-import './App.css';
-import Blog from './pages/Blog';
-import Home from './pages/Home';
-import { Routes, Route, Navigate } from 'react-router';
+// Define the main App component
 const App = () => {
   return (
-    <div className='container'>
+    <div className='container'> {/* Main container div for the entire application */}
     
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/blog/:id' element={<Blog />} />
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
+      {/* Set up routes using the Routes component */}
+      <Routes>
+        {/* Route for the home page */}
+        <Route path='/' element={<Home />} />
+        
+        {/* Route for individual blog posts with a dynamic parameter ':id' */}
+        <Route path='/blog/:id' element={<Blog />} />
+        
+        {/* Wildcard route to redirect undefined paths to the home page */}
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
       
     </div>
   );
 };
 
+// Export the App component as the default export of this module
 export default App;
