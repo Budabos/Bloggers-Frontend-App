d0a1fdfca028cff0f1e8e8657c1ba2404596d44d
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 23:15:30 +0300

    Create a reusable Chip component to represent a simple chip element

--- src/components/common/Chip/index.jsx
+++ src/components/common/Chip/index.jsx
@@ -1,6 +1,12 @@
+// Chip Component
+// This component represents a simple chip element with a label.
+
+// Import React and the component-specific styles
 import React from 'react';
 import './styles.css';
 
+// Define the Chip component, which takes a 'label' prop
 const Chip = ({ label }) => <p className='chip'>{label}</p>;
 
+// Export the Chip component as the default export of this module
 export default Chip;
