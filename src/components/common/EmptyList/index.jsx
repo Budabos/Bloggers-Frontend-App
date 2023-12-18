23aebc31bd1c00cb4a81dc176f2fd8f8fe8f5d58
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 23:18:10 +0300

    Implement EmptyList component

--- src/components/common/EmptyList/index.jsx
+++ src/components/common/EmptyList/index.jsx
@@ -1,10 +1,16 @@
+// EmptyList Component
+// This component represents a visual indication of an empty list.
+
+// Import React and the component-specific styles
 import React from 'react';
 import './styles.css';
 
+// Define the EmptyList component
 const EmptyList = () => (
   <div className='emptyList-wrap'>
     <img src='/assets/images/13525-empty.gif' alt='empty' />
   </div>
 );
 
+// Export the EmptyList component as the default export of this module
 export default EmptyList;
