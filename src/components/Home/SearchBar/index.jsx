f8fa66396314ce832a13e3d141ad7a77e34c7304
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-18 23:24:14 +0300

    Create a reusable SearchBar component for filtering content by category

--- src/components/Home/SearchBar/index.jsx
+++ src/components/Home/SearchBar/index.jsx
@@ -1,19 +1,30 @@
+// SearchBar Component
+// This component represents a search bar for filtering content by category.
+
+// Import React and the component-specific styles
 import React from 'react';
 import './styles.css';
 
+// Define the SearchBar component, which takes several props for functionality
 const SearchBar = ({ formSubmit, value, handleSearchKey, clearSearch }) => (
   <div className='searchBar-wrap'>
     <form onSubmit={formSubmit}>
+      {/* Input field for entering search keywords */}
       <input
         type='text'
         placeholder='Search By Category'
         value={value}
         onChange={handleSearchKey}
       />
+      
+      {/* Display 'X' button to clear search input if value is present */}
       {value && <span onClick={clearSearch}>X</span>}
+      
+      {/* Submit button for initiating the search */}
       <button>Go</button>
     </form>
   </div>
 );
 
+// Export the SearchBar component as the default export of this module
 export default SearchBar;
